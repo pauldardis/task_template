c04e4571bdad7a3ab21dcacc09fdca5dc146151a
pauldardis <paul.dardis@bt.com>
2019-12-26 18:56:24 +0000

    adding templates

--- app.py
+++ app.py
@@ -12,8 +12,16 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", 
+                            tasks=mongo.db.tasks.find())
 
+
+@app.route('/add_task')
+def add_task():
+    return render_template ('addtask.html', 
+                            categories=mongo.db.categories.find())
+
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=(os.environ.get('PORT')),
